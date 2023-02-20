e1d1f28394c3d6805d0ec10b3b08e0f4949fe74d
Jefferson Oliveira <56940973+jefbeauty@users.noreply.github.com>
2023-02-20 00:31:49 -0300

    maxupdatefev
    20.02.2023

---
+++ _js/script.js
@@ -0,0 +1,23 @@
+console.log("script");
+
+//alert("Max Portões");
+
+import React, { useState, useEffect } from 'react';
+
+function FriendStatus(props) {
+    const [isOnline, setIsOnline] = useState(null);
+    useEffect(() => {
+        function handleStatusChange(status) {
+            setIsOnline(status.isOnline);
+        }
+        ChatAPI.subscribeToFriendStatus(props.friend.id, handleStatusChange);
+        return () => {
+            ChatAPI.unsubscribeFromFriendStatus(props.friend.id, handleStatusChange);
+        };
+    });
+
+    if (isOnline === null) {
+        return 'Loading...';
+    }
+    return isOnline ? 'Online' : 'Offline';
+}
